7d0b95d8aab6792be0597e21d061548d641f30c5
Ivan <ivan241578@gmail.com>
2022-05-01 16:48:05 +0300

    Поиск max и min из двух чисел

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max;
+int min;
+if (a>b)
+{
+    max=a;
+    min=b;
+}
+else
+{
+    max=b;
+    min=a;
+}
+Console.WriteLine("max = " + max);
+Console.WriteLine("min = " + min);
